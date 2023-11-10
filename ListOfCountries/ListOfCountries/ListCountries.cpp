fb7af4af168d065c93fbf9d90966284f6e201b68
(Student  C00289302) Eliza Zimmermann <C00289302@setu.ie>
2023-11-10 09:59:54 +0000

    SLN

---
+++ ListOfCountries/ListOfCountries/ListCountries.cpp
@@ -0,0 +1,46 @@
+//Name: Vit Prechtl
+//Login: C00298335
+//Date: 10/11/2023
+
+//Name: Eliza Zimmermann
+//Login: C00289302
+//Date: 10/11/23
+
+
+//pick a letter from a to something
+//if (letter == a || ==A)
+//if region == 1
+//"Aurstarlia"
+
+#include <iostream>
+
+int main()
+{
+	int letter = 0;
+	//Eli's :)
+
+	std::cout << "Choose a letter from A to C" << std::endl;
+	std::cin >> letter;
+	system("CLS");
+
+	//error checking
+	if (letter != a || letter != A)
+	{
+		std::cout << "Error" << std::endl;
+	}
+
+
+	//If A
+	if (letter == a || letter == A)
+	{
+		std::cout << "Region 1" << std::endl;
+		std::cout << ""
+	}
+
+
+
+
+
+	//Vit's
+
+}
