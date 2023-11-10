15c00d352cbde5f1974f8fd0820ad3f4f146a545
(Student  C00289302) Eliza Zimmermann <C00289302@setu.ie>
2023-11-10 09:41:26 +0000

    Firstfunction

--- ListOfCountries.cpp
+++ ListOfCountries.cpp
@@ -5,3 +5,25 @@
 //Name: Eliza Zimmermann
 //Login: C00289302
 //Date: 10/11/23
+
+
+//pick a letter from a to something
+//if (letter == a || ==A)
+//if region == 1
+//"Aurstarlia"
+
+#include <iostream>
+
+int main()
+{
+	//Eli's
+
+
+
+
+
+
+	
+	//Vit's
+
+}
